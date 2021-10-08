198b09860af3818690e7a6c4fd2d2633dc8cd4c1
jarbus <jarbus@tutanota.com>
2021-10-08 16:36:54 -0400

    Removed bugged code, replaced with fetch.py

--- server/tab_parser.py
+++ server/tab_parser.py
@@ -21,18 +21,3 @@ def json_from_ultimate_tab(url: str) -> json:
     tab_dict = dict_from_ultimate_tab(url)
     data = json.dumps(tab_dict, ensure_ascii=False)
     return data
-
-
-if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-    except:
-        print('INCORRECT USAGE\n')
-        print('  Usage:')
-        print('    python %s {url}' % sys.argv[0])
-        sys.exit()
-
-    json_data = json_from_ultimate_tab(url)
-
-    pretty_format_json = json.dumps(json.loads(json_data), indent=4, sort_keys=True)
-    print(pretty_format_json)
